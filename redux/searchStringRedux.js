d8d8a136521443f5bbed5b938a0b8c95ca795f9b
Jakub Haliniak <jakubh.fake@gmail.com>
2021-09-17 11:03:34 +0200

    Problems with count cadrs -  countVisible

--- redux/searchStringRedux.js
+++ redux/searchStringRedux.js
@@ -2,9 +2,8 @@ import shortid from 'shortid';
 
 // selectors
 export const getSearchString = ({searchString}) => searchString;
-export const countAllCards= ({cards}) => cards.length;
+export const countAllCards = ({cards}) => cards.length;
 export const countVisibleCards = ({cards}, searchString) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).length;
-
 
 // action name creator
 const reducerName = 'stringSearch';
